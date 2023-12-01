296e4b88258744ebe450c4fe800fa682a0034693
MishaZ MSU_VMK <vbif1520@gmail.com>
2023-12-01 10:58:20 +0300

    binary tree whith leaks

--- sem_3/task_8/class.h
+++ sem_3/task_8/class.h
@@ -16,6 +16,7 @@ class Node{
     Node  *left;
     Node *right;
     Node(T value);
+//     bool color;
 
 };
   public:
@@ -27,7 +28,11 @@ class Node{
     ~RBtree();
     void delete_tree(Node * curr);
     void insert(Node * curr,T key);
-    Node * search(Node * curr,T key);
+    void printTree(Node * curr);
+    Node * search(Node * curr,T key); 
+    Node * getMin(Node * curr); 
+    Node * getMax(Node * curr); 
+    Node * delet(Node * curr,T key);
     // friend std::ostream &operator<<(std::ostream &out, Darr &x1);
 
 };
@@ -40,6 +45,7 @@ template<class T> RBtree<T>::Node::Node(T value)
   key=value;
   left=nullptr;
   right=nullptr;
+//   color=false;
 }
 
 template<class T> RBtree<T>::RBtree(T key)
@@ -75,26 +81,89 @@ template<class T> void RBtree<T>::insert(RBtree<T>::Node * curr,T key)
     }
 }
 
-template<class T> RBtree<T>::Node * RBtree<T>::search(RBtree<T>::Node * curr, T key)
+template<class T> void RBtree<T>::printTree(RBtree<T>::Node * curr)
+{
+    if(curr==nullptr) return;
+    printTree(curr->left);
+    cout<<curr->key<<" ";
+    printTree(curr->right);
+}
+
+template<class T> typename RBtree<T>::Node * RBtree<T>::search(RBtree<T>::Node * curr, T key)
 {
     if (curr == nullptr) return nullptr; 
     if(curr->key==key) return curr;
     return (key<curr->key)? search(curr->left,key) : search(curr->right,key);   
 }
 
-// template<class T> RBtree<T>::Node * RBtree<T>::getMin(RBtree<T>::Node * curr)
-// {
-//     if (curr == nullptr) return nullptr; 
-//     if(curr->left==nullptr) return curr;
-//     return getMin(curr->left);   
-// }
-
-// template<class T> RBtree<T>::Node * RBtree<T>::getMax(RBtree<T>::Node * curr)
-// {
-//     if (curr == nullptr) return nullptr; 
-//     if(curr->right==nullptr) return curr;
-//     return getMax(curr->right);   
+template<class T> typename RBtree<T>::Node * RBtree<T>::getMin(RBtree<T>::Node * curr)
+{
+    if (curr == nullptr) return nullptr; 
+    if(curr->left==nullptr) return curr;
+    return getMin(curr->left);   
 }
+
+template<class T>  typename RBtree<T>::Node * RBtree<T>::getMax(RBtree<T>::Node * curr)
+{
+    if (curr == nullptr) return nullptr; 
+    if(curr->right==nullptr) return curr;
+    return getMax(curr->right);   
+}
+
+template<class T> typename RBtree<T>::Node * RBtree<T>::delet(RBtree<T>::Node * curr, T key)
+{
+    if (curr == nullptr) return nullptr; 
+    else if(key < curr->key) curr->left = delet(curr->left,key);
+    else if(key > curr->key) curr->right = delet(curr->right,key);
+    else 
+    {
+      if(curr->left == nullptr || curr->right == nullptr)
+          curr=(curr->left==nullptr)?curr->right:curr->left;
+      else 
+      {
+        RBtree<T>::Node * maxInLeft = getMax(curr->left);
+        curr->key = maxInLeft->key;
+        curr->right = delet(curr->right,maxInLeft->key);
+        curr->left = maxInLeft->left;
+//         delete maxInLeft;
+      }
+    }
+    return curr;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- sem_3/task_8/main.cpp
+++ sem_3/task_8/main.cpp
@@ -4,9 +4,22 @@ using namespace std;
 
 int main()
 {  
-  RBtree<double> m_tree(1);
-  m_tree.insert(m_tree.root,2);
+  RBtree<double> m_tree(6);
+  m_tree.printTree(m_tree.root);
+  cout<<endl;
+  m_tree.insert(m_tree.root,4);
+  m_tree.printTree(m_tree.root);
+  cout<<endl;
+  m_tree.insert(m_tree.root,8);
+  m_tree.printTree(m_tree.root);
+  cout<<endl;
+  m_tree.insert(m_tree.root,7);
+  m_tree.printTree(m_tree.root);
+  cout<<endl;
 
+  m_tree.delet(m_tree.root,4);
+  m_tree.printTree(m_tree.root);
+  cout<<endl;
   // cout<<"Enter the number of autotests: ";
 
   return 0;
